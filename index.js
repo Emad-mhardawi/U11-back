f8970d3aab45bfaf6d9bd2529198fbdc4eda55ea
Emad Mhardawi <emad.mhardawi@gmail.com>
2021-09-17 06:39:38 +0200

    make it better

--- index.js
+++ index.js
@@ -28,4 +28,4 @@ app.use(errorHandler)
 
 connectDb();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}` ))
+app.listen(PORT,'0.0.0.0', console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}` ))
